f0dea17fcdb015a9d564b7114c405d902940bd27
LeonardoIGZ <leoguz079@hotmail.com>
2022-02-26 13:06:32 -0600

    Initial commit

---
+++ app/src/main/java/com/ligz/boucingball/MainActivity.java
@@ -0,0 +1,27 @@
+package com.ligz.boucingball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
